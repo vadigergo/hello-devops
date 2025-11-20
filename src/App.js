58744590bf09e8f22d298909ddd030768f83a3cd
Vadi Gergő <vadig@gyulaph.local>
2025-11-20 13:21:08 +0100

    Kezdeti commit – Hello DevOps app létrehozva

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello DevOps világ! 👋</h1>
+        <p>Ez az első saját React alapú DevOps alkalmazásom.</p>
       </header>
     </div>
   );
